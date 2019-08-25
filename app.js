cccbc718c76657488f72103854c3a7346fa065d3
Elton Griffith <ahmunre@yahoo.com>
2019-08-25 19:25:50 -0400

    added app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+$("#js-shopping-list-form").submit(function(event) {
+   let groceryItems = document.getElementById("shopping-list-entry").value;
+   document.forms["js-shopping-list-form"].reset();
+
+   $(".shopping-list").prepend(
+    `<li>
+   <span class="shopping-item">` + groceryItems + `</span>
+   <div class="shopping-item-controls">
+     <button class="shopping-item-toggle">
+       <span class="button-label">check</span>
+     </button>
+     <button class="shopping-item-delete">
+       <span class="button-label">delete</span>
+     </button>
+   </div>
+ </li>`);
+ event.preventDefault();
+
+});
+
+$("ul").on("click", ".shopping-item-toggle", function() {
+    $(this).closest("li").find(".shopping-item").toggleClass("shopping-item__checked");
+});
+
+$("ul").on("click", ".shopping-item-delete", function() {
+    $(this).closest("li").remove();
+});
